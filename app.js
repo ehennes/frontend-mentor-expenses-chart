c4f281783eacab44bbbc39826143e43671646527
emhennes <emilyhennes1@gmail.com>
2024-02-22 12:41:41 -0800

    Add styling for desktop and above.

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 3000;
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
