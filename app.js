efc29dbaa2f8a952d59010be3d0d39c3fa378722
emhennes <emilyhennes1@gmail.com>
2024-02-21 16:21:47 -0800

    Remove design files from project folder and update scaffolding of project.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
