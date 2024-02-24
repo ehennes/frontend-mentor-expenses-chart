b3dfe4e475cb4d8a065e714777615e5408610461
emhennes <emilyhennes1@gmail.com>
2024-02-24 12:53:13 -0800

    Update file structure.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
